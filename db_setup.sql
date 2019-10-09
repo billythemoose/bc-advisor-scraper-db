b5b31afc857387cb0b8f3f1975f3a6334b7e47b4
billythemoose <mdiegothomas@gmail.com>
2019-10-09 12:04:15 -0700

    Added additional tables

--- db_setup.sql
+++ db_setup.sql
@@ -1,30 +1,73 @@
+
 /*
 -- Drop table commands
-DROP TABLE college_programs;
+DROP TABLE student_trans;
+DROP TABLE student_info;
+DROP TABLE class_by_quarter;
+DROP TABLE classes;
+DROP TABLE individual_programs;
+DROP TABLE fields_of_study;
 DROP TABLE colleges;
 */
 
 -- Table Setup
 -- Table containing University/College names
-CREATE TABLE colleges(
+CREATE TABLE college(
 	id SERIAL PRIMARY KEY,
 	name VARCHAR(100)
 );
 
--- Table containing programs of study at universities 
-CREATE TABLE college_programs(
+-- Table containing fields of study
+CREATE TABLE fields_of_study(
 	id SERIAL PRIMARY KEY,
 	name VARCHAR(100),
-	college INTEGER REFERENCES colleges(id)
+	program_id VARCHAR(10) UNIQUE,
+	parent_college INTEGER REFERENCES college(id)
 );
 
-CREATE TABLE available_quarters(
+-- Table containing programs of study at universities 
+CREATE TABLE individual_programs(
 	id SERIAL PRIMARY KEY,
+	name VARCHAR(100),
+	program_id VARCHAR(10) UNIQUE,
+	parent_field VARCHAR(10) REFERENCES fields_of_study(program_id)
+);
+
+-- Table containing all classes
+CREATE TABLE classes(
+	id SERIAL PRIMARY KEY,
+	name VARCHAR(50),
+	class_id VARCHAR(10) UNIQUE
+);
+
+-- Table containg possible quarters
+CREATE TABLE class_by_quarter(
+	id SERIAL PRIMARY KEY,
+	class_id VARCHAR(10) REFERENCES classes(class_id),
 	year INTEGER,
 	quarter VARCHAR CHECK (quarter IN ('Spring', 'Summer', 'Fall', 'Winter'))
 );
 
+-- Table containing student information
+CREATE TABLE student_info(
+	student_id INTEGER PRIMARY KEY,
+	name VARCHAR(50),
+	program_id VARCHAR(10) REFERENCES individual_programs(program_id),
+	transcript_id INTEGER UNIQUE NOT NULL
+);
+
+-- Table containing student transcripts
+CREATE TABLE student_trans(
+	transcript_id INTEGER PRIMARY KEY REFERENCES student_info(transcript_id),
+	year INTEGER,
+	quarter VARCHAR CHECK (quarter IN ('Spring', 'Summer', 'Fall', 'Winter')),
+	class_id VARCHAR(10) REFERENCES classes(class_id)
+);
+
+
+/*
 -- Data Entry
 INSERT INTO available_quarters(year, quarter)
 VALUES 
 	(2019, 'Spring');
+*/
